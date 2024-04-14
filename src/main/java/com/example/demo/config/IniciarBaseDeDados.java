a3f772da311d00370a3b40824a269c817d9ff22d
Matheus <matheush.zehetmeyr@outlook.com>
2024-04-13 21:00:26 -0300

    Criando entity Pedido com Cliente aninhado

--- src/main/java/com/example/demo/config/IniciarBaseDeDados.java
+++ src/main/java/com/example/demo/config/IniciarBaseDeDados.java
@@ -1,16 +1,23 @@
 package com.example.demo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.example.demo.domain.Cliente;
+import com.example.demo.domain.Pedido;
 import com.example.demo.repository.ClienteRepository;
+import com.example.demo.repository.PedidoRepository;
 
 @Configuration
 public class IniciarBaseDeDados implements CommandLineRunner{
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
 	
 	@Autowired
 	private ClienteRepository clienteRepository;
@@ -18,13 +25,20 @@ public class IniciarBaseDeDados implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		clienteRepository.deleteAll();
 		
 		Cliente yuri = new Cliente(null, "Yuri", "exemplo2@gmail.com", "123456780", "01/01/1999");
 		Cliente gabriel = new Cliente(null, "Gabriel", "exemplo@gmail.com", "123456789", "01/01/2000");
 		
+		Pedido pedido1 = new Pedido(null, sdf.parse("13/04/24"), "Iphone 13", 3800.0, "256Gb de memoria", gabriel);
+		
+		Pedido pedido2 = new Pedido(null, sdf.parse("10/04/24"), "A54", 1500.0, "128Gb de memoria e 6Gb de RAM", yuri);
+		
 		clienteRepository.saveAll(Arrays.asList(yuri, gabriel));
-	
+		pedidoRepository.saveAll(Arrays.asList(pedido1, pedido2));
 	}
 	
 }
---
+++ src/main/java/com/example/demo/domain/Pedido.java
@@ -0,0 +1,102 @@
+package com.example.demo.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Pedido implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private String id;
+	private Date data;
+	private String nome;
+	private double preco;
+	private String descricao;
+	private Cliente cliente;
+	
+	public Pedido() {
+		
+	}
+
+	public Pedido(String id, Date data, String nome, double preco, String descricao, Cliente cliente) {
+		super();
+		this.id = id;
+		this.data = data;
+		this.nome = nome;
+		this.preco = preco;
+		this.descricao = descricao;
+		this.cliente = cliente;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pedido other = (Pedido) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
